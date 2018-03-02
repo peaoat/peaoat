b8c10d62d115e6396591305e1d59aa5b6199b388
extrafluous <squidsound@gmail.com>
2018-03-02 17:32:30 -0500

    123

--- js/ptandy.js
+++ js/ptandy.js
@@ -1,5 +1,9 @@
 var app = angular.module("ptandy", []);
 
+app.controller('headerCtrl', ['$scope', function($scope) {
+  $scope.title = 'henlo werld';
+}]);
+
 app.controller('pCtrl', ['$scope', function($scope) {
   $scope.hi = 'Hello World';
 }]);

8ba8070cd1df3fc91171e1d413203a845d54732f
extrafluous <squidsound@gmail.com>
2018-03-02 22:17:17 -0500

    123

--- js/ptandy.js
+++ js/ptandy.js
@@ -7,3 +7,10 @@ app.controller('headerCtrl', ['$scope', function($scope) {
 app.controller('pCtrl', ['$scope', function($scope) {
   $scope.hi = 'Hello World';
 }]);
+
+app.directive('volusionWidget', function() {
+  return {
+    restrict: 'E';
+    templateUrl: 'volusionWidget',
+  }
+})

50e3fcab9b3e55d532a766b6697d73854e49a195
extrafluous <squidsound@gmail.com>
2018-03-02 22:19:51 -0500

    123

--- js/ptandy.js
+++ js/ptandy.js
@@ -10,7 +10,7 @@ app.controller('pCtrl', ['$scope', function($scope) {
 
 app.directive('volusionWidget', function() {
   return {
-    restrict: 'E';
+    restrict: 'E',
     templateUrl: 'volusionWidget',
   }
-})
+});
